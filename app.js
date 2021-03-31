1bf81f638bf93d6c1867022e3cc574f7828dd76f
zl0ta <>
2021-03-31 23:38:16 +0300

    adding chats + sessions!

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const mysql = require("mysql2");
 const bcrypt = require("bcrypt");
 const express = require("express");
 const bodyParser = require("body-parser");
+const session = require('express-session');
 const WebSocket = require('ws');
 const http = require("http-server");
 
@@ -11,6 +12,15 @@ const wsServer = new WebSocket.Server({port: 3000});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//sessions
+app.use(session({
+  key: 'session_cookie_name',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { MaxAge: 30000 },
+  secret: 'session_cookie_secret'
+}))
 
 wsServer.on('connection', ws => {
   ws.on('message', m => {
@@ -25,8 +35,6 @@ wsServer.on('connection', ws => {
 server.listen(3030, function() {
   console.log("Server has been started...");
 });
-//app.use(express.cookieDecoder());
-//app.use(express.session());
 
 const pool = mysql.createPool({
     host: "localhost",
@@ -36,15 +44,20 @@ const pool = mysql.createPool({
     port: "3306"
 });
 
-//app.set("view engine", "hbs");
-
 //переключение между страницами
+app.get("/", function (req, res) {   
+  res.redirect("/main");
+});
+
 app.get("/reg", function (req, res) {   
   res.sendFile(__dirname + "/sign_up.html");
 });
 
-app.get("/main", function (req, res) {   
-  res.sendFile(__dirname + "/index.html");
+app.get("/main", function (req, res) {
+  if (req.session.authenticated) {
+    res.sendFile(__dirname + "/index.html");
+  }
+  else res.redirect("/log_in");
 });
 
 app.get("/log_in", function (req, res) {   
@@ -58,9 +71,13 @@ app.post("/reg", function (req, res) {
   const password = req.body.password;
   const hash = bcrypt.hashSync(password, 10);
   pool.query("INSERT INTO users (login, password) VALUES (?,?)", [login, hash], function(err) {
-    if(err) return console.log(err);
-    res.redirect("/main");
-  });
+    if(err) return res.send("Wrong data, try to change login.");
+    pool.query("SELECT id FROM users WHERE login = ?", [login], (err, rows) => {
+      req.session.authenticated = true;
+      req.session.userID = rows[0].id;
+      res.redirect("/main");
+    });
+  })
 });
 
 //вход в аккаунт, проверка 
@@ -71,17 +88,20 @@ app.post("/log_in", function (req, res) {
   
   (function pass_check() {
     //достаём из бд хэш пароля, соответствующий введённому логину
-    pool.query("SELECT password FROM users WHERE login = ?", [login], (err, rows) => {
-      if (err) return console.log(err.message);
+    pool.query("SELECT id, password FROM users WHERE login = ?", [login], (err, rows) => {
+      if (err) return res.send("Wrong data, try again or sign up first.");
       //проверяем правильность введённого пароля
-      bcrypt.compare(password, rows[0].password)
-      .then(result => {
-        if (result) {
-          //если true -> на главную страницу
-          res.redirect("/main");
-        }
-        else alert("Password is incorrect");
+      else {
+        bcrypt.compare(password, rows[0].password)
+        .then(result => {
+          if (result) {
+            req.session.authenticated = true;
+            req.session.userID = rows[0].id;
+            res.redirect("/main");
+          }
+          else alert("Password is incorrect");
       });
+      }
     });
   })();
 });
@@ -90,23 +110,25 @@ app.post("/main/add-chat", function (req, res) {
   if(!req.body) return res.sendStatus(400);
   const title = req.body.title;
   const description = req.body.description;
-  const user_id = 3;
-  pool.query("INSERT INTO chats (title, members, description) VALUES (?,?.?)", [title, user_id, description], function(err) {
+  const UID = JSON.stringify(req.session.userID);
+  pool.query("INSERT INTO chats (title, members, description) VALUES (?,?,?)", [title, UID, description], function(err) {
     if(err) return console.log(err);
     res.redirect("/main");
   });
 });
 
-/*app.post("/main", function (req, res) {
+app.post("/main/send", function (req, res) {
   if(!req.body) return res.sendStatus(400);
 
   const messages = req.body.messages;
-  const form = req.body.form;
+  const form = req.body;
   const input = req.body.input;
 
-  const ws = new WebSocket('ws://localhost:3000');
+  console.log(form);
 
-  function printMessage(value) {
+  //const ws = new WebSocket('ws://localhost:3000');
+
+  /*function printMessage(value) {
       const li = req.body.createElement('li');
       li.innerHTML = value;
       messages.appendChild(li);
@@ -115,11 +137,11 @@ app.post("/main/add-chat", function (req, res) {
   form.addEventListener('submit', event => {
       console.log(event);
       event.preventDefault();
-      ws.send(input.value);
+      wsServer.send(input.value);
       input.value = '';
   });
 
-  ws.onmessage = response => printMessage(response.data);
-});*/
+  wsServer.onmessage = response => printMessage(response.data);*/
+});
 
 app.listen(8080);
